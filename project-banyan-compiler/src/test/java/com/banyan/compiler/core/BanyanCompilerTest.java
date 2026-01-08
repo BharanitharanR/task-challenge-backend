f9be494792da9edc0b6603fa769af3471208f2c1
BharanitharanR <bharani_ran@yahoo.com>
2026-01-08 23:46:45 +0530

    unittests

--- project-banyan-compiler/src/test/java/com/banyan/compiler/core/BanyanCompilerTest.java
+++ project-banyan-compiler/src/test/java/com/banyan/compiler/core/BanyanCompilerTest.java
@@ -1,81 +1,85 @@
-package com.banyan.compiler.schema;
+package com.banyan.compiler.core;
 
-import com.banyan.compiler.core.BanyanCompiler;
-import com.banyan.compiler.core.CompilationResult;
 import com.banyan.compiler.pipeline.EvidenceTypeCompilationPipeline;
 import com.banyan.compiler.pipeline.RuleCompilationPipeline;
 import com.banyan.compiler.pipeline.RuleSetCompilationPipeline;
 import com.banyan.compiler.registry.CompilationPipelineRegistry;
+import com.banyan.compiler.testutil.JsonAssert;
 import com.banyan.compiler.testutil.TestResourceLoader;
+import com.google.common.cache.RemovalListener;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class RulesetSchemaValidatorTest {
-    private final RuleSetSchemaValidator validator =
-            new RuleSetSchemaValidator();
-
-    private static final String VALID_RESOURCE = "ruleset/schema/valid";
-    private static final String INVALID_RESOURCE = "ruleset/schema/invalid";
-
+public class BanyanCompilerTest {
     public CompilationPipelineRegistry registry = new CompilationPipelineRegistry();
     public BanyanCompiler compiler;
-
-    public RulesetSchemaValidatorTest(){
+    public BanyanCompilerTest(){
         this.registry.register("Rule",new RuleCompilationPipeline());
         this.registry.register("Ruleset",new RuleSetCompilationPipeline());
         this.registry.register("EvidenceType",new EvidenceTypeCompilationPipeline());
         this.compiler = new BanyanCompiler(this.registry);
     }
 
+
+    private static final String EVIDENCE_VALID_RESOURCE = "evidence-type/schema/valid";
+    private static final String EVIDENCE_INVALID_RESOURCE = "evidence-type/schema/invalid";
+
+    private static final String RULE_VALID_RESOURCE = "rule/schema/valid";
+    private static final String RULE_INVALID_RESOURCE = "rule/schema/invalid";
+
     @Test
     void allValidRulesShouldPass() {
         List<String> jsons =
-                TestResourceLoader.loadJsonFiles(VALID_RESOURCE);
-
+                TestResourceLoader.loadJsonFiles(RULE_VALID_RESOURCE);
         for (String json : jsons) {
-            List<String> errors = validator.validate(json);
-            assertTrue(errors.isEmpty(), "Expected no errors but got: " + errors);
-        }
-    }
-
-    @Test
-    void allInvalidRulesShouldFail() {
-        List<String> jsons =
-                TestResourceLoader.loadJsonFiles(INVALID_RESOURCE);
-
-        for (String json : jsons) {
-            System.out.println(json);
-            List<String> errors = validator.validate(json);
-            errors.forEach(System.out::println);
-            assertFalse(errors.isEmpty(), "Expected errors but got none");
-        }
-    }
-
-    @Test
-    void allCompilerCallShouldPass() {
-        List<String> jsons =
-                TestResourceLoader.loadJsonFiles(VALID_RESOURCE);
-
-        for (String json : jsons) {
-
             CompilationResult result = this.compiler.compile(json);
+            System.out.println(json);
             result.getErrors().forEach(System.out::println);
             assertTrue(result.isSuccess());
         }
     }
 
     @Test
-    void allCompilerCallShouldFail() {
+    void allInvalidRulesShouldFail() {
         List<String> jsons =
-                TestResourceLoader.loadJsonFiles(INVALID_RESOURCE);
+                TestResourceLoader.loadJsonFiles(RULE_INVALID_RESOURCE);
+
         for (String json : jsons) {
+
             CompilationResult result = this.compiler.compile(json);
-            result.getErrors().forEach(System.out::println);
             assertTrue(!result.isSuccess());
         }
     }
+
+    @Test
+    void allValidTypesShouldPass() {
+        List<String> jsons =
+                TestResourceLoader.loadJsonFiles(EVIDENCE_VALID_RESOURCE);
+
+        for (String json : jsons) {
+
+            CompilationResult result = this.compiler.compile(json);
+            assertTrue(result.isSuccess());
+        }
+    }
+
+    @Test
+    void allInvalidEvidenceTypesShouldFail() {
+        List<String> jsons =
+                TestResourceLoader.loadJsonFiles(EVIDENCE_INVALID_RESOURCE);
+
+        for (String json : jsons) {
+
+            CompilationResult result = this.compiler.compile(json);
+            assertTrue(!result.isSuccess());
+        }
+    }
+
+
 }
